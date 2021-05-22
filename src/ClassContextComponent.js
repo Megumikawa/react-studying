46cd69c82adef8af7db8df8ad837ea9f16b63da4
Megumikawa <megumi19901016@gmail.com>
2021-05-22 18:44:08 +0200

    created base for learning ContextComponent

--- src/ClassContextComponent.js
+++ src/ClassContextComponent.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { ThemeContext } from './LearnUseContext';
 
-export default function ClassContextComponent() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default class ClassContextComponent extends Component {
+  themeStyles(dark) {
+    return {
+      backgroundColor: dark ? '#333' : '#CCC',
+      color: dark ? '#CCC' : '#333',
+      padding:'2rem',
+      margin: '2rem'
+    }
+  }
+
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {darkTheme => {
+          return <div style={this.themeStyles(darkTheme)}>Class Theme</div>
+        }}
+      </ThemeContext.Consumer>
+    )
+  }
 }
