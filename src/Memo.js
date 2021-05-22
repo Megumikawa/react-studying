6cd096bad4049121ef02405337de50ba09708e1a
Megumikawa <megumi19901016@gmail.com>
2021-05-22 15:56:46 +0200

    finished learning useMemo

--- src/Memo.js
+++ src/Memo.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo, useEffect } from 'react'
 
 export default function Memo() {
   const [number, setNumber] = useState(0)
   const [dark, setDark] = useState(false)
-  const doubleNumber = slowFunction(number)
-  const themeStyles = {
-    backgroundColor: dark ? 'black' : 'white',
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number)
+  }, [number])
+
+  const themeStyles = useMemo(() =>{
+    return {
+      backgroundColor: dark ? 'black' : 'white',
     color: dark ? 'white' : 'black'
-  }
+    }
+  }, [dark])
+
+  useEffect(() => {
+    console.log('Theme Changed')
+  }, [themeStyles])
 
   return (
     <>
