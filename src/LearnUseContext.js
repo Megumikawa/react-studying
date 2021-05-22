1eb63ed0145abd2f2729cca92743b85f088976ea
Megumikawa <megumi19901016@gmail.com>
2021-05-22 21:43:22 +0200

    finished learning useContext

--- src/LearnUseContext.js
+++ src/LearnUseContext.js
@@ -1,23 +1,11 @@
 import React, { useState } from 'react'
 import FunctionContextComponent from './FuntionContextComponent'
-import ClassContextComponent from './ClassContextComponent'
-
-export const ThemeContext = React.createContext()
+import { ThemeProvider } from './ThemeContext'
 
 export default function LearnUseContext() {
-  const [darkTheme, setDarkTheme] = useState(true)
-
-  function toggleTheme() {
-    setDarkTheme(prevDarkTheme => !prevDarkTheme)
-  }
-
   return (
-    <>
-      <ThemeContext.Provider value={darkTheme}>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-        <FunctionContextComponent />
-        <ClassContextComponent />
-      </ThemeContext.Provider>
-    </>
+    <ThemeProvider>
+      <FunctionContextComponent />
+    </ThemeProvider>
   )
 }
