ab73c6933d87349eb82fda414f647fbbd90ad9ba
Megumikawa <megumi19901016@gmail.com>
2021-05-22 17:58:03 +0200

    finished learning useRef

--- src/LearnUseRef.js
+++ src/LearnUseRef.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function LearnUseRef() {
   const [name, setName] =useState('')
+  const prevName = useRef('')
+
+  useEffect(() => {
+    prevName.current = name
+  }, [name])
+
   return (
     <>
       <input value={name} onChange={e => setName(e.target.value)} />
-      <div>My name is {name}</div>
+      <div>My name is {name} and it used to {prevName.current}</div>
     </>
   )
 }
